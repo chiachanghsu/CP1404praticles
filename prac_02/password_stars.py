b7afaaf5263b1213b1c3463d2e6a0f948ff88263
xujia <chiachang.hsu@my.jcu.edu.au>
2024-05-28 14:42:30 +0800

    refactor get_password

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,17 @@
 MINIMUN_LENGTH = 10
 
 
-password = input("Enter your password: ")
-while len(password) < MINIMUN_LENGTH:
-    print("too short.")
+def main():
+    password = get_password()
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter your password: ")
-print("*" * len(password))
+    while len(password) < MINIMUN_LENGTH:
+        print("too short.")
+        password = input("Enter your password: ")
+    return password
+
+
+main()

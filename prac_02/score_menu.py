1c56833fe8b9cd3aa4302c885f380bc0622c4b7d
xujia <chiachang.hsu@my.jcu.edu.au>
2024-05-28 15:28:19 +0800

    add get_valid_score function to score_menu.py

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -9,16 +9,24 @@ def main():
     choice = input("Choice: ").upper()
     while choice != "Q":
         if choice == "G":
-            score = get_vaild_score()
-            print(f"Your score is {score}")
+            score = get_valid_score()
+            print(f"Your score is {score}.")
         elif choice == "P":
-            if score == "":
-                print("Where is your score?")
-            else:
-                grade = determine_grade(score)
-                print(f"Your grade is {grade}")
+            grade = determine_grade(score)
+            print(f"Your grade is {grade}.")
         elif choice == "S":
             print("*" * score)
         print(MENU)
         choice = input("Choice: ").upper()
     print("Farewell.")
+
+
+def get_valid_score():
+    score = int(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid.")
+        score = int(input("Enter score: "))
+    return score
+
+
+main()

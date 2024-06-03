aba154881f42f6fd794e5b4ca0497407026c6904
xujia <chiachang.hsu@my.jcu.edu.au>
2024-06-03 15:34:56 +0800

    finish randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+# line 1: smallest: 5 largest: 20
+# line 2: smallest: 3 largest: 9 NO
+# line 3: smallest: 2.588538024168905 largest: 5.454736523669478
+import random
+number = random.randint(1, 100)
+print(number)

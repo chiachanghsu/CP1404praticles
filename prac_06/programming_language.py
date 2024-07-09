880132e8dddd146c91579f5c19e4cca16c4f188f
xujia <chiachang.hsu@my.jcu.edu.au>
2024-07-09 10:55:46 +0800

    add docstring

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,12 +3,14 @@
 class ProgrammingLanguage:
 
     def __init__(self, name="", typing="", reflection=True, year=0):
+        """Initialise a language instance."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Determine if the language is dynamic."""
         return self.typing == "Dynamic"
 
     def __str__(self):

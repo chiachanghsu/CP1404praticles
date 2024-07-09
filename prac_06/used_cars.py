cb17be72b0714fd1c0761ae1041d9dd9ab21abfd
xujia <chiachang.hsu@my.jcu.edu.au>
2024-07-09 09:21:07 +0800

    finished used_car.py

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -13,6 +13,11 @@ def main():
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+    limo = Car(100, "limo")
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo)
 
 
 main()

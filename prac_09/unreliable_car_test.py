aef536584b471609b709675d241ba494a6fc0ab4
xujia <chiachang.hsu@my.jcu.edu.au>
2024-07-29 15:37:50 +0800

    rename the constant

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -2,11 +2,11 @@ import random
 from prac_09.unreliable_car import UnreliableCar
 STARTING_VALUE = 0
 ENDING_VALUE = 100
-DRIVING_VALUE = 40
+DISTANCE = 40
 
 
 my_car = UnreliableCar("Prius 2", 100, 50.0)
 # print(my_car)
 if random.randint(STARTING_VALUE, ENDING_VALUE) < my_car.reliability:
-    my_car.drive(DRIVING_VALUE)
+    my_car.drive(DISTANCE)
 print(my_car)

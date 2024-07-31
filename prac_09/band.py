7b1321861446da53fbe0d6db8fd67fe14343e4b0
xujia <chiachang.hsu@my.jcu.edu.au>
2024-07-31 13:49:26 +0800

    complete play part

--- prac_09/band.py
+++ prac_09/band.py
@@ -5,18 +5,27 @@ class Band(Musician):
 
     def __init__(self, band_name):
         super().__init__()
-        self.band_info = []
         self.band_name = band_name
         self.band_info = ''
+        self.band_lists = []
 
     def add(self, band_team):
         self.name = band_team.name
-        self.instruments = band_team.instruments
+        self.band_lists.append(band_team)
         if self.band_info == '':
             self.band_info = Musician.__str__(band_team)
         else:
             self.band_info += ', ' + Musician.__str__(band_team)
-        print(self.band_info)
 
     def __str__(self):
         return f'{self.band_name} ({self.band_info})'
+
+    def play(self):
+        for band_list in self.band_lists:
+            self.name = band_list.name
+            self.instruments = band_list.instruments
+            if not self.instruments:
+                print(f"{self.name} needs an instrument!")
+            else:
+                print(f"{self.name} is playing: {self.instruments[0]}")
+        return ''

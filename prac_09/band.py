8f4ad2dc3b01fd4e86c1e04a559af885aa1153cb
xujia <chiachang.hsu@my.jcu.edu.au>
2024-07-31 13:21:55 +0800

    complete __str__ part

--- prac_09/band.py
+++ prac_09/band.py
@@ -5,13 +5,18 @@ class Band(Musician):
 
     def __init__(self, band_name):
         super().__init__()
+        self.band_info = []
         self.band_name = band_name
-        self.band_lists = []
+        self.band_info = ''
 
     def add(self, band_team):
         self.name = band_team.name
         self.instruments = band_team.instruments
-        self.band_lists.append(band_team)
+        if self.band_info == '':
+            self.band_info = Musician.__str__(band_team)
+        else:
+            self.band_info += ', ' + Musician.__str__(band_team)
+        print(self.band_info)
 
     def __str__(self):
-        return f'{self.band_name} ({self.band_lists})'
+        return f'{self.band_name} ({self.band_info})'
